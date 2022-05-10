8b013bdbfb26e04a1f0f28c41484c78c727dcf80
Tess <njerigitundu003@gmail.com>
2022-05-10 17:40:44 +0300

    Create first route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,22 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
 
+        get("/animals", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Animal> animals = Animal.all();
+            model.put("animals", animals);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }

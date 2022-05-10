3c6b8f33faa525736ad96f36c844180d207277af
Tess <njerigitundu003@gmail.com>
2022-05-11 01:38:16 +0300

    Make adjustments on pom.xml file

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,5 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "tess", "Njeri@Postgres");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgres://ylymowtlnioklz:02dbb8c85e701306cae688ef1fa49e4c95fd79a774316b643d2bb025a842a002@ec2-35-168-194-15.compute-1.amazonaws.com:5432/d3f87p3gc0b20m", "ylymowtlnioklz", "02dbb8c85e701306cae688ef1fa49e4c95fd79a774316b643d2bb025a842a002");
 }
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -6,7 +6,7 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "tess", "Njeri@Postgres");
+        DB.sql2o = new Sql2o("jdbc:postgres://ylymowtlnioklz:02dbb8c85e701306cae688ef1fa49e4c95fd79a774316b643d2bb025a842a002@ec2-35-168-194-15.compute-1.amazonaws.com:5432/d3f87p3gc0b20m", "ylymowtlnioklz", "02dbb8c85e701306cae688ef1fa49e4c95fd79a774316b643d2bb025a842a002");
     }
 
     @Override
